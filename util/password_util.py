30e572c5171697445e678c4901bc145a7307ace9
Jeffery Adorkor <jead@v-p-s.com>
2023-03-25 22:23:03 +0100

    move password and secrets functions to helper

--- util/password_util.py
+++
@@ -1,7 +0,0 @@
-from passlib.context import CryptContext
-
-password_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
-
-
-def hash_password(password: str):
-    return password_context.hash(password)
