9cd7815010af401f93ad035872f8cde6348fa4ac
Jeffery Adorkor <jead@v-p-s.com>
2023-03-25 22:23:49 +0100

    switch to different database for dev

--- config/database_config.py
+++ config/database_config.py
@@ -2,13 +2,13 @@ from pprint import pprint
 
 from pymongo import MongoClient
 
-from util.get_secrets import secrets
+from helper.secrets_helper import secrets
 
 
 def connect_to_database():
     try:
         mongo_client = MongoClient(secrets.get('MONGODB_URL_DEV'))
-        database = mongo_client["test"]
+        database = mongo_client["fastapi"]
         print('Connected to MongoDB')
         return database
     except ConnectionError as e:
