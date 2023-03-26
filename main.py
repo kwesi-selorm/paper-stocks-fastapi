1e85042e74e5b0a20d9ef6736044ee18099bd4ec
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2023-03-27 00:56:35 +0200

    create asset router

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
-from router import user_router
+from router import user_router, asset_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allowed_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
 app.include_router(user_router.router)
+app.include_router(asset_router.router)
 
 
 @app.get("/")
--- router/user_router.py
+++ router/user_router.py
@@ -1,11 +1,9 @@
 import json
 from typing import Annotated
 
-from fastapi import APIRouter, HTTPException, Depends, Body, status
+from fastapi import APIRouter, HTTPException, Depends, Body
 
 from auth.jwt_fns import create_access_token, verify_access_token
-from helper.symbol_helper import verify_symbol
-from model.AssetModel import BuyAssetInput, Asset
 from model.UserModel import SignInUser, SignUpUser, User
 from service.AssetService import AssetService
 from service.UserService import UserService
@@ -104,46 +102,3 @@ async def get_user(user_id: str):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail={"message": str(e)})
-
-
-@router.post("/buy-asset/{user_id}", dependencies=[Depends(verify_access_token)], status_code=status.HTTP_201_CREATED)
-async def buy_asset(user_id: str, buy_asset_input: Annotated[BuyAssetInput, Body(required=True)]):
-    symbol = buy_asset_input.dict().get("symbol")
-    name = buy_asset_input.dict().get("name")
-    new_position = buy_asset_input.dict().get("position")
-    new_amount_invested = buy_asset_input.dict().get("amountInvested")
-
-    user_doc = user_service.find_by_id(user_id)
-
-    if not user_doc:
-        raise HTTPException(status_code=404, detail={"message": f"User with id '{user_id}' found"})
-
-    verify_symbol(symbol)
-    remaining_buying_power = user_doc.get("buyingPower") - new_amount_invested
-    already_owned_asset = asset_service.find_by_user_id_and_symbol(user_id, symbol)
-
-    if already_owned_asset is None:
-        asset_dict = {"symbol": symbol, "name": name, "position": new_position, "amountInvested": new_amount_invested,
-                      "averagePrice": new_amount_invested / new_position, "userId": user_id}
-        new_asset = Asset(**asset_dict)
-
-        try:
-            asset_service.save_new(new_asset)
-            user_service.update_on_buy(user_id, {"buyingPower": remaining_buying_power})
-            return new_asset
-        except Exception as e:
-            raise HTTPException(status_code=500, detail={"message": "Error completing the transaction" + str(e)})
-
-    else:
-        asset_id = already_owned_asset.get("_id")
-        old_position = already_owned_asset.get("position")
-        old_amount_invested = already_owned_asset.get("amountInvested")
-        update = asset_service.find_position_and_average_on_buy(
-            old_position, old_amount_invested, new_position, new_amount_invested)
-
-        try:
-            updated_asset = asset_service.update_on_buy(asset_id, update)
-            user_service.update_on_buy(user_id, {"buyingPower": remaining_buying_power})
-            return ReturnedAsset(symbol=updated_asset.get("symbol"), position=updated_asset.get("position"))
-        except Exception as e:
-            raise HTTPException(status_code=500, detail={"message": "Error completing the transaction" + str(e)})
