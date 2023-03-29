69ffb36fa802a7bb4e1f6230b00ffad04d9f10c9
Jeffery Adorkor <jead@v-p-s.com>
2023-03-29 16:28:19 +0200

    move try-except out

--- config/DatabaseConfig.py
+++ config/DatabaseConfig.py
@@ -10,10 +10,6 @@ load_dotenv()
 class DatabaseConfig:
     @staticmethod
     def get_collection(collection_name):
-        try:
-            mongo_client = MongoClient(os.environ.get("MONGODB_URL"))
-            database = mongo_client["fastapi"]
-            return database[collection_name]
-        except ConnectionError as e:
-            print('Failed to connect to MongoDB', str(e))
-            return JSONResponse(status_code=500, content={"message": "Failed to connect to MongoDB"})
+        mongo_client = MongoClient(os.environ.get("MONGODB_URL"))
+        database = mongo_client["fastapi"]
+        return database[collection_name]
